5f0e53d88d4ebf05f344282edd002c98eca75060
freelancerjoynal <developerjoynal@gmail.com>
2024-06-13 16:35:19 +0600

    before protect section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
         "lime-green": "#243c5a",
         "gray-aspargus": "#3A5548",
         "myrtle-green": "#3A5548",
-        "gun-mental": "#3A5548",
+        "gun-mental": "#4F5155",
         emerald: "#51CC47",
       },
     },
