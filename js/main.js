0ca39096fa5538ca4f7538b8dcc3ce62d007102c
freelancerjoynal <developerjoynal@gmail.com>
2024-06-13 19:54:42 +0600

    dropdown features done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  //=================================
+  //Mobile menu toggle
+  //=================================
+
   $("#menuOpen").click(function () {
     $("#mobileMenu").css("left", "0");
     $(this).hide();
@@ -8,5 +12,24 @@ $(document).ready(function () {
     $(this).hide();
     $("#menuOpen").show();
     $("#mobileMenu").css("left", "101%");
+  });
+
+  //=================================
+  // Toggle dropdown visibility
+  //=================================
+  $("#interest").click(function () {
+    $("#interest-dropdown").toggle();
+    $("#residence-dropdown").hide();
+    $("#door-window-dropdown").hide();
+  });
+  $("#residence").click(function () {
+    $("#residence-dropdown").toggle();
+    $("#interest-dropdown").hide();
+    $("#door-window-dropdown").hide();
+  });
+  $("#door-window").click(function () {
+    $("#door-window-dropdown").toggle();
+    $("#interest-dropdown").hide();
+    $("#residence-dropdown").hide();
   });
 });
