70521a979763cd1c4f6fea34fed8549b6b317bec
freelancerjoynal <developerjoynal@gmail.com>
2024-06-14 16:22:01 +0600

    just modal created with daisyui

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "lime-green": "#243c5a",
         "gray-aspargus": "#3A5548",
         "jungle-green": "#24725F",
+        charcoal: "#23252A",
         "gun-mental": "#4F5155",
         emerald: "#51CC47",
       },
